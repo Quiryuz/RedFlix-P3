e722d2148332b266bbcbf3786665695de23c813a
Facundo Garrido <1507facu@gmail.com>
2026-06-07 15:33:06 -0300

    Fix de inicialización de Bootstrap corrigiendo bundles de MVC, orden de scripts en Layout y errores de minificación WebGrease. Se agrego un dropdown del modulo administrativo visible solo para rol admin

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RedFlix.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            if (Session["rolID"] == null || Convert.ToInt32(Session["rolID"]) != 1)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Controllers/PeliculasController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RedFlix.Controllers
+{
+    public class PeliculasController : Controller
+    {
+        // GET: Peliculas
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       /* public ActionResult Populares();
+        public ActionResult Tendencias();
+        public ActionResult Detalle(int id);
+        public ActionResult Buscar(string nombre);*/
+    }
+}
